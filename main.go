a58f6f69ca79f8cf71a06c7031406243212bbe1a
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2020-01-02 23:29:26 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package area
+
+import "math"
+
+// Pi e um programa programa o programa que e o maior programa
+const Pi = 3.1452
+
+// Cir e um programa programa o programa lsque e o maior programa
+func Cir(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}
+
+// React e um programa programa o programa que e o maior programa
+func React(base, altura float64) float64 {
+	return base * altura
+}
+
+func _Triangulo(base, altura float64) float64 {
+	return base * altura
+}
